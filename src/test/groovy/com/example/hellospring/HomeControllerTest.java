0e0469315bbaee6396d250a59c5e745b94bfb822
Julian Serrano <julian.serrano@viewnext.com>
2021-08-04 12:07:28 +0200

    Fixed uncompleted homeController test.

--- src/test/groovy/com/example/hellospring/HomeControllerTest.java
+++ src/test/groovy/com/example/hellospring/HomeControllerTest.java
@@ -1,17 +1,25 @@
 package com.example.hellospring;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HomeControllerTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
-    void helloController() {
+    void homeController() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/hola", String.class))
+                .isEqualTo("Hola");
     }
 }
